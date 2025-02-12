206464c1c1e6ae116b198664e0397687d60d05e3
Satyendra Kumar <satyendra.xcrino@gmail.com>
2025-02-12 18:14:15 +0530

    a

--- resources/views/builderpanel/welcome.blade.php
+++ resources/views/builderpanel/welcome.blade.php
@@ -11,15 +11,15 @@
                       <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                           <div class="card-content">
-                            <h5 class="font-15">Rules</h5>
-                            <h2 class="mb-3 font-18">12</h2>
+                            {{-- <h5 class="font-15">Rules</h5>
+                            <h2 class="mb-3 font-18">12</h2> --}}
                             {{-- <p class="mb-0"><span class="col-green">10%</span> Increase</p> --}}
                           </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
-                          <div class="banner-img">
+                          {{-- <div class="banner-img">
                             <img src="{{asset("assets/img/banner/1.png")}}" alt="">
-                          </div>
+                          </div> --}}
                         </div>
                       </div>
                     </div>
@@ -33,15 +33,15 @@
                       <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                           <div class="card-content">
-                            <h5 class="font-15"> Builders</h5>
-                            <h2 class="mb-3 font-18">1,7</h2>
+                            {{-- <h5 class="font-15"> Builders</h5>
+                            <h2 class="mb-3 font-18">1,7</h2> --}}
                             {{-- <p class="mb-0"><span class="col-orange">09%</span> Decrease</p> --}}
                           </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
-                          <div class="banner-img">
+                          {{-- <div class="banner-img">
                             <img src="{{asset("assets\img\users\bu.jpg")}}" alt="">
-                          </div>
+                          </div> --}}
                         </div>
                       </div>
                     </div>
@@ -55,16 +55,16 @@
                       <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                           <div class="card-content">
-                            <h5 class="font-15">Projects</h5>
-                            <h2 class="mb-3 font-18">18</h2>
+                            {{-- <h5 class="font-15">Projects</h5>
+                            <h2 class="mb-3 font-18">18</h2> --}}
                             {{-- <p class="mb-0"><span class="col-green">18%</span>
                               Increase</p> --}}
                           </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
-                          <div class="banner-img">
+                          {{-- <div class="banner-img">
                             <img src="{{asset("assets\img\users\project.avif")}}" alt="">
-                          </div>
+                          </div> --}}
                         </div>
                       </div>
                     </div>
@@ -78,15 +78,15 @@
                       <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                           <div class="card-content">
-                            <h5 class="font-15">Rejected</h5>
-                            <h2 class="mb-3 font-18">97</h2>
+                            {{-- <h5 class="font-15">Rejected</h5>
+                            <h2 class="mb-3 font-18">97</h2> --}}
                             {{-- <p class="mb-0"><span class="col-green">42%</span> Increase</p> --}}
                           </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
-                          <div class="banner-img">
+                          {{-- <div class="banner-img">
                             <img src="{{asset("assets/img/banner/4.png")}}" alt="">
-                          </div>
+                          </div> --}}
                         </div>
                       </div>
                     </div>
